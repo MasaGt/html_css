7760bd0dc2ea4d67bf17ca6eeac38cc66615f0b1
MasaGt <gachapin2236tx@gmail.com>
2024-07-16 13:18:27 +1200

    Add another sample of three.js

--- JavaScript/Library/threejs/samples/cube_ball/index.js
+++ JavaScript/Library/threejs/samples/cube_ball/index.js
@@ -31,10 +31,33 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const planeGeometry = new THREE.PlaneGeometry(60, 20);
 const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-// planeMesh.rotation.z = -0.5 * Math.PI;
-
+planeMesh.rotation.x = -0.5 * Math.PI;
 // オブジェクトをシーンに追加
 scene.add(planeMesh);
+
+// ボックスのオブジェクトの作成
+const cubeGeometry = new THREE.BoxGeometry(4, 4, 4); // 幅、高さ、奥行きが全て4
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+}); // 赤色で骨組みが見えるように
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+cubeMesh.position.x = -4;
+cubeMesh.position.y = 3;
+cubeMesh.position.z = 0;
+scene.add(cubeMesh);
+
+// ボールオブジェクトの追加
+const sphereGeometry = new THREE.SphereGeometry(4, 20, 20); //半径が4で、水平方向、垂直方向にしようされるセグメントの数がどちらも20
+const sphereMaterial = new THREE.MeshBasicMaterial({
+  color: 0x7777ff,
+  wireframe: true,
+}); //青色で骨組みが見えるように
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.x = 20;
+sphere.position.y = 4;
+sphere.position.z = 2;
+scene.add(sphere);
 
 // レンダリング
 renderer.render(scene, camera);
